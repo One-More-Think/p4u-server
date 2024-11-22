cb7bcfe83c97bdd4745c330b1d168f831a7022a7
younghoonyou <kcnoh2@hanyang.ac.kr>
2024-11-21 20:33:30 -0800

    feat: create template for user login

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from 'users/users.controller';
 import { UsersService } from 'users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
+import { User } from 'entity/user.entity';
+import { RedisService } from 'redis/redis.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), HttpModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RedisService, ConfigService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'entity/user.entity';
+import { LoginDto, PostUserDto } from 'users/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOneUser(id: string): Promise<User> {
+    try {
+      return await this.usersRepository.findOneBy({ id });
+    } catch (err) {
+      throw new Error('Failed to find user');
+    }
+  }
+
+  async loginUser(sns: string, data: PostUserDto): Promise<LoginDto> {
+    try {
+      // WIP implement login user
+      let res: LoginDto;
+      return res;
+    } catch (err) {
+      throw new Error('Failed to create user');
+    }
+  }
+
+  async removeUser(id: number): Promise<void> {
+    try {
+      await this.usersRepository.delete(id);
+    } catch (err) {
+      throw new Error('Failed to delete user');
+    }
+  }
+}
