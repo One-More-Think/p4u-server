86e7b79c6c8d236da3656d6a59ab78910dc94aef
Minho Lee <minho.lee0716@gmail.com>
2025-01-20 08:19:15 +0900

    fix: API end-point

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Param,
   UseGuards,
   ParseIntPipe,
+  ForbiddenException,
 } from '@nestjs/common';
 import { UsersService } from 'users/users.service';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -25,7 +26,7 @@ import { AccessTokenPayload } from 'auth/types';
 @ApiTags('User')
 @Controller({ path: 'users' })
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @ApiOperation({ summary: 'Admin Login' })
   @Post('sign-in/admin')
@@ -58,19 +59,23 @@ export class UsersController {
   @ApiOperation({ summary: 'User update' })
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @Put()
+  @Put(':userId')
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateUser(
+    @Param('userId', ParseIntPipe) userId: number,
     @User() user: AccessTokenPayload,
     @Body() dto: UpdateUserDto,
   ) {
+    if (user.id !== userId) {
+      throw new ForbiddenException('Invalid user id');
+    }
     await this.usersService.updateUser(user.id, dto);
   }
 
   @ApiOperation({ summary: 'User detail' })
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @Get('detail/:userId')
+  @Get(':userId')
   async getUserDetail(@Param('userId', ParseIntPipe) userId: number) {
     return await this.usersService.getUserDetail(userId);
   }
